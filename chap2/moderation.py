607aff2d972d764e2a7ca1d17c86c363d51d412e
Marie-Alice Blete <marie-alice.blete@worldline.com>
2023-06-25 09:57:49 +0200

    Revert "add chaps 2, 4 and 5"
    This reverts commit a60c3de4c106fe22215def21343914243637b04c.

--- chap2/moderation.py
+++
@@ -1,9 +0,0 @@
-import openai
-
-response = openai.Moderation.create(
-    model='text-moderation-latest',
-    input="I want to kill my neighbor.",
-)
-
-print(response['results'][0]['category_scores'])
-
